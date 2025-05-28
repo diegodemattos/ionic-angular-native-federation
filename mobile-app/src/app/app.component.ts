8729e429540bde0415be0fe9b713912d674f092f
Diego de Mattos <75817309+diegodemattos@users.noreply.github.com>
2025-05-28 18:00:49 -0300

    feature: configure mfes on mobile app

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonApp, IonRouterOutlet, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonContent, IonApp, IonRouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   constructor() {}
