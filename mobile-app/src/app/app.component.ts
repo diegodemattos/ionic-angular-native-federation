2699bb100c3e6f5952a65c2aed996c71d4e8ba54
Diego de Mattos <75817309+diegodemattos@users.noreply.github.com>
2025-05-28 18:33:58 -0300

    feature: configure mfes to display correctly on mobile

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonContent } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonContent, IonApp, IonRouterOutlet],
+  styleUrls: ['./app.component.scss'],
+  imports: [IonApp, IonRouterOutlet],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
